1b3498eeeb78f946be1ff18e442ddaad83291a17
Henry Ell <henrydell4793@gmail.com>
2021-09-22 23:32:41 -0500

    Only commit on this studio

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -33,3 +33,4 @@ public class BalancedBrackets {
         return brackets == 0;
     }
 }
+
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,7 @@
 package test;
 
+
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +14,58 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void  onlyBalancedBracketsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("]["));
+    }
 
+    @Test
+    public void  leftBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void rightBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void wordBetweenBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[launched]"));
+    }
+
+    @Test
+    public void wordBetweenTurnedBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("]launched["));
+    }
+
+    @Test
+    public void wordBetweenInnerBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("land[]lock"));
+    }
+
+    @Test
+    public void wordAfterBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]launched"));
+    }
+
+    @Test
+    public void wordFrontBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("launched[]"));
+    }
+
+    @Test
+    public void wordAfterABracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[launched"));
+    }
+
+    @Test
+    public void wordBeforeABracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("launched]"));
+    }
 }
+
+
